9127d4ef1c4a289429a9219236e1df9248398869
burakdmb <burakhan.demirbilek@gmail.com>
2019-02-24 14:59:52 +0300

    Updated controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -2,15 +2,20 @@ import socket
 import ConfigParser
 import time
 import OPi.GPIO as GPIO
+import os
+import signal
+import sys
 
 
 def main():
-
-    GPIO.setmode(GPIO.BOARD)
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    
+    
+    initGPIO()
     initPin(12)
     setPinHigh(12)
 
-    
     configParser = ConfigParser.RawConfigParser()   
     configFilePath = r'config.txt'
     configParser.read(configFilePath)
@@ -23,26 +28,41 @@ def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     client.bind(("", 5000))
+    print('Press Ctrl+C to quit')
     while True:
         data, addr = client.recvfrom(1024)
         parseData(data)
 
+    GPIO.cleanup()
     client.close()
 
+def signal_handler(sig, frame):
+        cleanupGPIO()
+        sys.exit(0)
+
+def initGPIO():
+    if socket.gethostname() == 'orangepizero':
+        GPIO.setmode(GPIO.BOARD)
 
 #Initializes the pins to output function
 #all of the pins which are going to be used needs to be initialized.
 def initPin(pinNo):
-    GPIO.setup(pinNo, GPIO.OUT)
+    if socket.gethostname() == 'orangepizero':
+        GPIO.setup(pinNo, GPIO.OUT)
 
 
 #Sets to logic high(3.3V)
 def setPinHigh(pinNo):
-    GPIO.output(pinNo, GPIO.HIGH)
+    if socket.gethostname() == 'orangepizero':
+        GPIO.output(pinNo, GPIO.HIGH)
 
 #Sets to logic low(0V)
 def setPinLow(pinNo):
     GPIO.output(pinNo,GPIO.LOW)
+
+def cleanupGPIO():
+    if socket.gethostname() == 'orangepizero':
+        GPIO.cleanup()
 def parseData(data):
     rawStr = data.split(",")
     devNo = rawStr[0].split(":")[1]
