87d684d96ad4219188eb124f2351013d529234ce
burakdmb <burakhan.demirbilek@gmail.com>
2019-02-25 02:32:44 +0300

    Updated brain.py

--- brain/box_follower_video.py
+++ brain/box_follower_video.py
@@ -14,8 +14,10 @@ def main():
     vs = VideoStream(src=0).start()
     time.sleep(1.0)
 
-    lower = (0, 0, 0)
-    upper = (255, 255, 255)
+    lowerRobot = (0, 0, 0)
+    upperRobot = (255, 255, 255)
+    lowerObstacle = (0, 0, 0)
+    upperObstacle = (255, 255, 255)
 
     while True:
         image = vs.read()
@@ -24,34 +26,47 @@ def main():
 
         k = cv2.waitKey(5) & 0xFF
         if (k == 27):
-            print(lower, upper)
             break
         if (k==97):
             robotLow, robotHigh, obstacleLow, obstacleHigh= configureColorRange(image, hsv)
-            lower=(int(robotLow[0]), int(robotLow[1]), int(robotLow[2]))
-            upper=(int(robotHigh[0]), int(robotHigh[1]), int(robotHigh[2]))
+            lowerRobot=(int(robotLow[0]), int(robotLow[1]), int(robotLow[2]))
+            upperRobot=(int(robotHigh[0]), int(robotHigh[1]), int(robotHigh[2]))
+            lowerObstacle=(int(obstacleLow[0]), int(obstacleLow[1]), int(obstacleLow[2]))
+            upperObstacle=(int(obstacleHigh[0]), int(obstacleHigh[1]), int(obstacleHigh[2]))
             cv2.destroyAllWindows()
         #image=filterImageForRobot(image, hsv, lower, upper)
-        mask = cv2.inRange(hsv, lower, upper)   
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+        mask = cv2.inRange(hsv.copy(), lowerRobot, upperRobot)   
+        kernel = np.ones((5,5),np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, None)
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cntsRobot = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        if len(cnts) >= 2:
-            cnts.sort(key=cv2.contourArea, reverse=True)
-            if (cv2.contourArea(cnts[0]) / cv2.contourArea(cnts[1])) < 5 and cv2.contourArea(cnts[0])>200:
-                #cnts,bbox=sort_contours(cnts[:2])
-                x1,y1 = getCenterOfBox(cnts[0])
-                x2,y2 = getCenterOfBox(cnts[1])
+        cntsRobot = imutils.grab_contours(cntsRobot)
+
+
+        maskObstacle = cv2.inRange(hsv.copy(), lowerObstacle, upperObstacle)   
+        maskObstacle = cv2.erode(maskObstacle, None, iterations=2)
+        maskObstacle = cv2.dilate(maskObstacle, None, iterations=2)
+        maskObstacle = cv2.morphologyEx(maskObstacle, cv2.MORPH_OPEN, None)
+        maskObstacle = cv2.morphologyEx(maskObstacle, cv2.MORPH_CLOSE, None)
+        cntsObstacle = cv2.findContours(maskObstacle.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cntsObstacle = imutils.grab_contours(cntsObstacle)
+        if len(cntsRobot) >= 2:
+            #Sort the contours by the area and check is it big enough to be a robot
+            cntsRobot.sort(key=cv2.contourArea, reverse=True)
+            if (cv2.contourArea(cntsRobot[0]) / cv2.contourArea(cntsRobot[1])) < 5 and cv2.contourArea(cntsRobot[0])>200:
+                #cntsRobot,bbox=sort_contours(cntsRobot[:2])
+                x1,y1 = getCenterOfBox(cntsRobot[0])
+                x2,y2 = getCenterOfBox(cntsRobot[1])
                 angle = int(math.degrees(math.atan2(y1-y2,x1-x2)))
-                drawBox(cnts[0], image, (x1,y1))
-                drawBox(cnts[1], image, (x2,y2))
+                drawBox(cntsRobot[0], image, (x1,y1))
+                drawBox(cntsRobot[1], image, (x2,y2))
                 cv2.putText(image,'Angle: '+str(angle),(30, 30), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-                if cv2.contourArea(cnts[0])> cv2.contourArea(cnts[1]):
+                if cv2.contourArea(cntsRobot[0])> cv2.contourArea(cntsRobot[1]):
                     #small box is the front of the robot and
                     #left box is bigger than the right box, so its facing the right side 
                     cv2.arrowedLine(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
@@ -59,10 +74,26 @@ def main():
                     #small box is the front of the robot and
                     #right box is bigger than the left box, so its facing the left side
                     cv2.arrowedLine(image, (x2, y2), (x1, y1), (255, 0, 0), 3)
-    
-        cv2.imshow('Robot Detector', image)
 
-        
+                
+                if len(cntsObstacle)>0:
+                    cntsObstacle.sort(key=cv2.contourArea, reverse=True)
+                    #check that there is an obstacle contour and its area is bigger than 200
+                    if cv2.contourArea(cntsObstacle[0])>200:
+
+                        x3,y3 = getCenterOfBox(cntsObstacle[0])
+                        angle2 = int(math.degrees(math.atan2(y1-y3,x1-x3)))
+                        cv2.putText(image,'Angle2: '+str(angle2),(30, 100), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+                        cv2.arrowedLine(image, (x1, y1), (x3, y3), (255, 0, 255), 3)
+                        decideAction(angle, angle2)
+
+
+                        
+        #newImg=cv2.bitwise_or(mask, maskObstacle)
+        cv2.imshow('Robot Detector: Press a to configure filters', image)
+
+    
     vs.stop()
     cv2.destroyAllWindows()
 
@@ -84,11 +115,13 @@ def configureColorRange(image, hsvImage):
     r2 = cv2.selectROI("Select obstacle color", image)
     obstacleImg = hsvImage[int(r2[1]):int(r2[1]+r2[3]), int(r2[0]):int(r2[0]+r2[2])]
 
-    robotLow, robotHigh, avgL, avgH= findRanges(robotImg)
-    obstacleLow, obstacleHigh, a,b = findRanges(obstacleImg)
+    robotLow, robotHigh, robotAvgL, robotAvgH= findRanges(robotImg)
+    obstacleLow, obstacleHigh, obstacleAvgL, obstacleAvgH = findRanges(obstacleImg)
     
+    #print( robotLow, robotHigh, obstacleLow, obstacleHigh)
     return robotLow, robotHigh, obstacleLow, obstacleHigh
-
+    #return robotAvgL, robotAvgH, obstacleAvgL, obstacleAvgH
+    
 def findRanges(image):
     lowH=255
     lowS=255
@@ -126,9 +159,20 @@ def findRanges(image):
         tmpS=tmpS/(count)
         tmpV=tmpV/(count)
 
-    return (max(0, lowH-30), max(0, lowS-75), max(0, lowV-75)), (min(255, highH+15), min(255, highS+50), min(255, highV+50)), (max(0, tmpH-30), max(0, tmpS-60), max(0, tmpV-60)), (min(255, tmpH+30), min(255, tmpS+30), min(255, tmpV+60))
+    return (max(0, lowH-10), max(0, lowS-10), max(0, lowV-10)), (min(255, highH+10), min(255, highS+10), min(255, highV+10)), (max(0, tmpH-30), max(0, tmpS-30), max(0, tmpV-30)), (min(255, tmpH+30), min(255, tmpS+30), min(255, tmpV+30))
 
 
+def decideAction(robotAngle, obstacleAngle):
+    angle= obstacleAngle- robotAngle
+    #if angle is between -15, +15 degrees, then move forward 
+    if angle>=-15 and angle<=15:
+        print("forward")
+    #else if angle is between 15,180 degrees then turn right
+    elif angle>15 and angle<180:
+        print("right")
+    #else if angle is between -15,-180 degrees then turn left
+    elif angle<-15 and angle>-180:
+        print("left")
 
 
 if __name__ == "__main__":
