c049d89242fe5ef958744da5ad29ec2439cc37f1
burakdmb <burakhan.demirbilek@gmail.com>
2019-04-10 08:25:59 +0300

    update brain

--- brain/brain.py
+++ brain/brain.py
@@ -268,7 +268,7 @@ class Brain():
                     self.findAllRobots(5, self.lowerRobot1, self.upperRobot1, self.lowerRobot2, self.upperRobot2, self.lowerRobot3, self.upperRobot3, \
                         self.lowerRobot4, self.upperRobot4, self.lowerObstacle, self.upperObstacle)
 
-            if listObsx != None:
+            if type(listObsx) != NoneType:
                 for i in range(len(listObsx)):
                     obstacles.append(Thing(Position(listObsx[i]),listObsy[i], 5, "Obstacle"))
             print("a1")
