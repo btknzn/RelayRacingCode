e8d29679863d79b10e2468e23a1259704a9fc935
burakdmb <burakhan.demirbilek@gmail.com>
2019-04-10 04:51:02 +0300

    Updated controller

--- controller/controller.py
+++ controller/controller.py
@@ -229,7 +229,7 @@ class PiController(Controller):
 
 def main():
     
-    controller = PiController(ip='192.168.43.37')
+    controller = PiController(ip='192.168.43.88')
     signal.signal(signal.SIGINT, controller.signal_handler)
     while not controller.closed:
         controller.run()
