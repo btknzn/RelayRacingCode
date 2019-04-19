ff92a3bd6807257f6a724a8bd0f6d3b49ecda599
burakdmb <burakhan.demirbilek@gmail.com>
2019-04-19 10:02:26 +0300

    Updated brain

--- brain/brain.py
+++ brain/brain.py
@@ -309,8 +309,8 @@ class Brain():
             # grid size
             
             
-            grid_size = 25.0  # potential grid size [m]
-            robot_radius = 10.0  # robot radius [m]
+            grid_size = 20.0  # potential grid size [m]
+            robot_radius = 5.0  # robot radius [m]
             rx1, ry1 = potential_field_planning(
                 x1, y1, x2, y2, listObsx, listObsy, grid_size, robot_radius)
             #came_from, cost_so_far, last = a_star_search(obstacles, robot2start, robot3end, 3, 4, 200)
