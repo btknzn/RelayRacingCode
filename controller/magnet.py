7630ac91a1c7b799f9baef7d8c524aa83d002bfb
burakdmb <burakhan.demirbilek@gmail.com>
2019-04-19 10:36:29 +0300

    added magnet

---
+++ controller/magnet.py
@@ -0,0 +1,25 @@
+
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+from time import sleep
+from orangepwm import *
+
+self.IN = port.PA15
+
+gpio.init()
+
+gpio.setcfg(self.IN, gpio.OUTPUT)
+
+magnet=False
+
+print("Press any key to switch:")
+x = input()
+
+while x != "q":
+    magnet = not magnet
+    if magnet:
+        gpio.output(self.IN, gpio.HIGH)
+    else:
+        gpio.output(self.IN, gpio.LOW)
+    print("Press any key to switch:")
+    x = input()
